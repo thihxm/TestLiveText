fe7dad4c7f7b185b3b5e510ac2c393532e02e3ae
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-09-01 14:18:55 -0300

    Initial Commit

---
+++ TestLiveText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestLiveText
+//
+//  Created by Thiago Medeiros on 01/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestLiveText/TestLiveTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLiveTextApp.swift
+//  TestLiveText
+//
+//  Created by Thiago Medeiros on 01/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TestLiveTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
